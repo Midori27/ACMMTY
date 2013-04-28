071b86dda54cf2941a677202e1b3fb0f2a821c8d
Juan Jose Lenero Lozano <juanjo.lenero@gmail.com>
2013-04-28 01:44:15 -0500

    filtroNoAuth

---
+++ src/java/Filtro/FiltroSinAuth.java
@@ -0,0 +1,28 @@
+/* HOLA */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the templato in the editor.
+ */
+
+package Filtro;
+
+import Modelo.Usuario;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author juanjo
+ */
+public class FiltroSinAuth extends FiltroAuth {
+
+    @Override
+    protected boolean isAuth(HttpServletRequest req) {
+        Usuario u = (Usuario) req.getSession().getAttribute(Usuario.NOMBRE_TABLA);
+        if(u==null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+}
