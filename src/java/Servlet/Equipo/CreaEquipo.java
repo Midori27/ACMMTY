a087c8ae154b11fe5c682c4b43fabeaa1c04da8b
Juan Jose Lenero Lozano <juanjo.lenero@gmail.com>
2013-05-01 17:52:46 -0500

    CleanAndBuild

--- src/java/Servlet/Equipo/CreaEquipo.java
+++ src/java/Servlet/Equipo/CreaEquipo.java
@@ -5,7 +5,9 @@
 package Servlet.Equipo;
 
 import Controlador.ParseaParametros;
+import Controlador.Query;
 import Modelo.Equipo;
+import Modelo.Evento;
 import Modelo.Usuario;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author juanjo
  */
 public class CreaEquipo extends HttpServlet {
+    public static final String URL_VISTA = "/WEB-INF/creaEquipo.jsp";
 
     /**
      * Processes requests for both HTTP
@@ -35,16 +38,26 @@ public class CreaEquipo extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         Usuario u = (Usuario)request.getSession().getAttribute(Usuario.NOMBRE_TABLA);
+        ArrayList<Integer> idUsuarios = new ArrayList<Integer>();
+        idUsuarios.add(u.getId());
         HashMap<String,String> parametros = ParseaParametros.parsea(request, this.getServletConfig().getServletContext());
+        Integer idEvento = Integer.parseInt(parametros.get(Equipo.COL_ID_EVENTO));
         String nombre = parametros.get(Equipo.COL_NOMBRE);
         String estado = "";
         int puntaje = 0;
         String clave = parametros.get(Equipo.COL_CLAVE);
-        Integer idEvento = Integer.parseInt(parametros.get(Equipo.COL_ID_EVENTO));
-        ArrayList<Integer> idUsuarios = new ArrayList<Integer>();
-        idUsuarios.add(puntaje);
+        Query q = new Query();
+        Evento evento = q.getEventoBD(idEvento);
+        Integer maxIntegrantes = evento.getMaxIntegrantesEquipo();
         
-        Equipo e = new Equipo(idEvento, idUsuarios, nombre, estado, puntaje, clave, maxIntegrantes)
+        Equipo equipo = new Equipo(idEvento, idUsuarios, nombre, estado, puntaje, clave, maxIntegrantes);
+        
+        if(q.insertaEquipoBD(equipo)){
+            request.setAttribute("mensaje", "Equipo creado exitosamente.");
+        }else{
+            request.setAttribute("mensaje", "Lo sentimos, el equipo no puede ser creado en este momento.");
+            request.getRequestDispatcher(URL_VISTA).forward(request,response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
